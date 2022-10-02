c878636bec708a2def81c5189d8ccf60aa0d530e
Jaewon <jungjaedev@gmail.com>
2022-10-02 21:10:57 +0900

    Feat: basic setup with cna

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Dev Blog</title>
+      </Head>
+      <h2>Hello</h2>
+    </div>
+  );
+}
