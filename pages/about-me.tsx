59d4b1810bea18aa67480c6e58e933642710ddd6
Jaewon <jungjaedev@gmail.com>
2022-10-13 01:02:27 +0900

    reset repository

--- pages/about-me.tsx
+++
@@ -1,7 +0,0 @@
-export default function AboutMe() {
-  return (
-    <div>
-      <h2>AboutMe</h2>
-    </div>
-  );
-}
--- pages/projects.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/layout'
-
-export default function Projects() {
-  return (
-    <Layout>
-      <h1>Projects</h1>
-    </Layout>
-  );
-}
