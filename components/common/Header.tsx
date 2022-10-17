cb4401fe2efa4a392fe8bad0a15dbf8a6f294f76
Jaewon <jungjaedev@gmail.com>
2022-10-17 12:21:56 +0900

    Fix: useRouter to Link tag

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,21 +1,18 @@
-import { useRouter } from "next/router";
+import Link from 'next/link'
 
 const Header = () => {
-  const router = useRouter();
 
-  const onMoveMainHandler = () => {
-    router.push("/");
-  };
   return (
-    <div className="hidden md:contents">
+    <div className="md:contents">
       <div className="sticky top-0 mx-auto max-w-[1080px] bg-white bg-opacity-50 py-[8px] px-4">
         <div className="flex justify-between py-2">
-          <div
-            onClick={onMoveMainHandler}
-            className="flex cursor-pointer items-center px-2 text-2xl font-bold text-black"
-          >
-            Tech.blog
-          </div>
+          <Link href="/">
+            <div
+              className="flex cursor-pointer items-center px-2 text-2xl font-bold text-black"
+            >
+              Tech.blog
+            </div>
+          </Link>
           {/* <div className="flex items-center space-x-8 px-2"> */}
             {/* <div>a</div> */}
             {/* <div>b</div> */}
--- components/post/PostList.tsx
+++ components/post/PostList.tsx
@@ -1,17 +1,18 @@
+import Link from 'next/link'
 import { Posts } from "types/posts";
-import { useRouter } from "next/router";
 import PostItem from 'components/post/PostItem'
 
 export const PostList = ({posts} : Posts) => {
-  const router = useRouter();
   return (
     <main>
       <div className="mx-auto grid max-w-[768px] gap-y-4 px-4 pb-[100px] md:max-w-[1080px] md:grid-cols-3 md:gap-x-6 md:gap-y-[30px]">
         {posts.map((post) => {
           return (
-            <div onClick={() => router.push(`/blog/${post.slug}`)} key={post.slug}>
-              <PostItem post={post.frontMatter} />
-            </div>
+            <Link key={post.slug} href={`/blog/${post.slug}`}>
+              <a>
+                <PostItem post={post.frontMatter} />
+              </a>
+            </Link>
           )
         })}
       </div>
