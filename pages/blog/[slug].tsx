d15d9f1d9de15eb0ff84fc100bdeb8730e5790cc
jungjaedev <jungjaedev@gmail.com>
2022-11-25 15:48:11 +0900

    Add: meta tags in Head component

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,26 +1,40 @@
-import fs from 'fs';
-import { GetStaticProps, GetStaticPaths, } from 'next';
-import matter from 'gray-matter'
-import { frontMatterProps  } from 'types/posts';
-import { marked } from 'marked'
-import PostTopSection from 'components/post/PostTopSection';
+import Head from "next/head";
+import fs from "fs";
+import { GetStaticProps, GetStaticPaths } from "next";
+import matter from "gray-matter";
+import { frontMatterProps } from "types/posts";
+import { marked } from "marked";
+import PostTopSection from "components/post/PostTopSection";
 
-interface BlogPageProps { 
+interface BlogPageProps {
   frontMatter: frontMatterProps;
   content: string;
 }
 
-const BlogPage = ({frontMatter, content}: BlogPageProps) => {
+const BlogPage = ({ frontMatter, content }: BlogPageProps) => {
+  console.log("frontmatter", frontMatter);
+  console.log("content", content);
   return (
-    <div className="mx-auto max-w-[1080px] px-4 md:px-2">
-      <PostTopSection frontMatter={frontMatter}/>
-      <div
-        className='prose-headings:text-black30 prose max-w-none prose-p:text-black20 prose-code:text-blue-400'
-        dangerouslySetInnerHTML={{ __html: marked(content) }}
-      />
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>{frontMatter.title}</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content={frontMatter.content} />
+        <meta name="description" content={frontMatter.tags} />
+        <meta property="og:image" content={frontMatter.thumbnail} />
+        <meta property="og:description" content={frontMatter.content} />
+        <meta property="og:title" content={frontMatter.title} />
+      </Head>
+      <div className="mx-auto max-w-[1080px] px-4 md:px-2">
+        <PostTopSection frontMatter={frontMatter} />
+        <div
+          className="prose-headings:text-black30 prose max-w-none prose-p:text-black20 prose-code:text-blue-400"
+          dangerouslySetInnerHTML={{ __html: marked(content) }}
+        />
+      </div>
+    </>
+  );
+};
 
 export default BlogPage;
 
@@ -36,8 +50,8 @@ export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths,
     fallback: false,
-  }
-}
+  };
+};
 
 export const getStaticProps: GetStaticProps = (context) => {
   // get content for each blog
@@ -48,6 +62,6 @@ export const getStaticProps: GetStaticProps = (context) => {
     props: {
       frontMatter,
       content,
-    }
-  }
-}
+    },
+  };
+};
